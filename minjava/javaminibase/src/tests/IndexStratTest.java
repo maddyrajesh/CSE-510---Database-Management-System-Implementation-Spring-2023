121bd320d7b2e7804b09b46b19b58744bd9e79de
dghowart <dhowarth161@gmail.com>
2023-03-22 19:40:45 -0700

    I updated the tests so all five of them are run. I also printed out the runtimes in milliseconds after all of them are done running.

--- minjava/javaminibase/src/tests/IndexStratTest.java
+++ minjava/javaminibase/src/tests/IndexStratTest.java
@@ -21,63 +21,34 @@ public class IndexStratTest {
 
     private static final int NUM_PAGES = 100000;
     private static int mapCount = 0;
+
+    // These are the private timer variables that are used in each test function.
+
     private static long test1Time = 0;
     private static long test2Time = 0;
     private static long test3Time = 0;
     private static long test4Time = 0;
     private static long test5Time = 0;
 
-    //   Create the five different databases, each of which will have a different index/clustering strategy and will be
-    // used in the comparison.
 
-    private static bigt database1;
-    private static bigt database2;
-    private static bigt database3;
-    private static bigt database4;
-    private static bigt database5;
+    //   This creates the databases that are used in the tests. These databases use the data from the given csv file
+    // on Ed's Discussion.
 
-
-    private static void init() {
+    private static void createDatabase(String datbaseName, String datatableName, int type) {
         Integer numPages = NUM_PAGES;
         pcounter.initialize();
 
         FileInputStream fileStream = null;
         BufferedReader br = null;
         try {
-            File f = new File("strat1.db");
-            new SystemDefs("strat1.db", numPages, NUMBUF, "Clock");
-            database1 = new bigt("index1", 1);
-
-/*
-            f = new File(basePath + "/tmp/strat2.db");
-            new SystemDefs(basePath + "/tmp/strat2.db", numPages, NUMBUF, "Clock");
-            database2 = new bigt("strat2.in", 2);
-
-
-            f = new File(basePath + "/tmp/strat3.db");
-            new SystemDefs(basePath + "/tmp/strat3.db", numPages, NUMBUF, "Clock");
-            database3 = new bigt("strat3.in", 3);
-
-
-            f = new File(basePath + "/tmp/strat4.db");
-            new SystemDefs(basePath + "/tmp/strat4.db", numPages, NUMBUF, "Clock");
-            database4 = new bigt("strat4.in", 4);
-
-*/
-            //File f = new File(basePath + "/strat5.db");
-            //new SystemDefs(basePath + "/strat5.db", numPages, NUMBUF, "Clock");
-            //bigt database5 = new bigt("strat5", 5);
+            File f = new File(datbaseName);
+            new SystemDefs(datbaseName, numPages, NUMBUF, "Clock");
+            bigt database1 = new bigt(datatableName, type);
             fileStream = new FileInputStream("test_data1.csv");
             br = new BufferedReader(new InputStreamReader(fileStream, "UTF-8"));
             String inputStr;
-            int mapCount = 0;
             while ((inputStr = br.readLine()) != null) {
                 String[] input = inputStr.split(",");
-                //set the map
-                //System.out.println(input[0]);
-               // System.out.println(input[1]);
-                //System.out.println(input[2]);
-                //System.out.println(input[3]);
                 Map map = new Map();
                 short strSizes[] = setBigTConstants("test_data1.csv");
                 AttrType attr[] = {new AttrType(AttrType.attrString),
@@ -89,16 +60,11 @@ public class IndexStratTest {
                 map.setColumnLabel(input[1]);
                 map.setTimeStamp(Integer.parseInt(input[2]));
                 map.setValue(input[3]);
-                ///System.out.println("row: " + map.getRowLabel());
-                //System.out.println("column: " + map.getColumnLabel());
-                //System.out.println("time: " + map.getTimeStamp());
+
                 MID mid = database1.insertMap(map.getMapByteArray());
-               // mid = database2.insertMap(map.getMapByteArray(), strSizes);
-                //mid = database3.insertMap(map.getMapByteArray(), strSizes);
-               // mid = database4.insertMap(map.getMapByteArray(), strSizes);
-                //MID mid = database5.insertMap(map.getMapByteArray());
                 mapCount++;
             }
+            System.out.println("Index strategy: " + type);
             System.out.println("=======================================\n");
             System.out.println("map count: " + database1.getMapCnt());
             System.out.println("Distinct Rows = " + database1.getRowCnt());
@@ -108,6 +74,7 @@ public class IndexStratTest {
             System.out.println("Writes: " + pcounter.wcounter);
             System.out.println("NumBUFS: " + NUMBUF);
             System.out.println("\n=======================================\n");
+            database1.close();
         } catch (InvalidMapSizeException e) {
             e.printStackTrace();
         } catch (ConstructPageException e) {
@@ -138,12 +105,13 @@ public class IndexStratTest {
 
     public static void test1() throws Exception {
         pcounter.initialize();
+        long tmpTime = System.nanoTime();
+        createDatabase("strat1.db", "index1", 1);
         bigt databaseTest = new bigt("index1", 1);
         assert(databaseTest.getMapCnt() == mapCount);
-        long tmpTime = System.nanoTime();
-        System.out.println("stream");
-        Stream stream = databaseTest.openStream(3, "Sweden", "Moose", "00002");
-        System.out.println("after stream");
+        test1Time = System.nanoTime();
+        test1Time -= tmpTime;
+        /*Stream stream = databaseTest.openStream(3, "Sweden", "Moose", "00002");
         MID mid = new MID();
         Map map = new Map();
         int numOfMatches = 0 ;
@@ -155,82 +123,115 @@ public class IndexStratTest {
             }
         }
         while(map != null);
-        test1Time = System.nanoTime();
-        test1Time -= tmpTime;
-        System.out.println("Reads : " + pcounter.rcounter);
-        System.out.println("Number of matches found: " + numOfMatches);
+        */
     }
 
 
     //  Test index strategy 2.
 
     public static void test2() throws Exception {
-        assert(database2.getMapCnt() == mapCount);
+        pcounter.initialize();
         long tmpTime = System.nanoTime();
-        Stream stream = database2.openStream(1, null, "Sweden", null);
-        MID mid = new MID();
-        Map map = new Map();
-        do {
-            map = stream.getNext(mid);
-        }
-        while(map != null);
+        createDatabase("strat2.db", "index2", 2);
+        bigt databaseTest = new bigt("index2", 2);
+        assert(databaseTest.getMapCnt() == mapCount);
         test2Time = System.nanoTime();
         test2Time -= tmpTime;
+        /*Stream stream = databaseTest.openStream(3, "Sweden", "Moose", "00002");
+        MID mid = new MID();
+        Map map = new Map();
+        int numOfMatches = 0 ;
+        do {
+            map = stream.getNext(mid);
+            if(map != null) {
+                numOfMatches++;
+                System.out.println(map.getColumnLabel());
+            }
+        }
+        while(map != null);
+        */
     }
 
 
     //  Test index strategy 3.
 
     public static void test3() throws Exception {
-        assert(database3.getMapCnt() == mapCount);
+        pcounter.initialize();
         long tmpTime = System.nanoTime();
-        Stream stream = database3.openStream(1, null, "Sweden", null);
-        MID mid = new MID();
-        Map map = new Map();
-        do {
-            map = stream.getNext(mid);
-        }
-        while(map != null);
+        createDatabase("strat3.db", "index3", 3);
+        bigt databaseTest = new bigt("index3", 3);
+        assert(databaseTest.getMapCnt() == mapCount);
         test3Time = System.nanoTime();
         test3Time -= tmpTime;
+        /*Stream stream = databaseTest.openStream(3, "Sweden", "Moose", "00002");
+        MID mid = new MID();
+        Map map = new Map();
+        int numOfMatches = 0 ;
+        do {
+            map = stream.getNext(mid);
+            if(map != null) {
+                numOfMatches++;
+                System.out.println(map.getColumnLabel());
+            }
+        }
+        while(map != null);
+        */
     }
 
 
     //  Test index strategy 4.
 
     public static void test4() throws Exception {
-        assert(database4.getMapCnt() == mapCount);
+        pcounter.initialize();
         long tmpTime = System.nanoTime();
-        Stream stream = database4.openStream(1, null, "Sweden", null);
-        MID mid = new MID();
-        Map map = new Map();
-        do {
-            map = stream.getNext(mid);
-        }
-        while(map != null);
+        createDatabase("strat4.db", "index4", 4);
+        bigt databaseTest = new bigt("index4", 4);
+        assert(databaseTest.getMapCnt() == mapCount);
         test4Time = System.nanoTime();
         test4Time -= tmpTime;
+        /*Stream stream = databaseTest.openStream(3, "Sweden", "Moose", "00002");
+        MID mid = new MID();
+        Map map = new Map();
+        int numOfMatches = 0 ;
+        do {
+            map = stream.getNext(mid);
+            if(map != null) {
+                numOfMatches++;
+                System.out.println(map.getColumnLabel());
+            }
+        }
+        while(map != null);
+        */
     }
 
 
     //  Test index strategy 5.
 
     public static void test5() throws Exception {
-        assert(database5.getMapCnt() == mapCount);
+        pcounter.initialize();
         long tmpTime = System.nanoTime();
-        Stream stream = database5.openStream(1, null, "Sweden", null);
-        MID mid = new MID();
-        Map map = new Map();
-        do {
-            map = stream.getNext(mid);
-        }
-        while(map != null);
+        createDatabase("strat5.db", "index5", 5);
+        bigt databaseTest = new bigt("index5", 5);
+        assert(databaseTest.getMapCnt() == mapCount);
         test5Time = System.nanoTime();
         test5Time -= tmpTime;
+        /*Stream stream = databaseTest.openStream(3, "Sweden", "Moose", "00002");
+        MID mid = new MID();
+        Map map = new Map();
+        int numOfMatches = 0 ;
+        do {
+            map = stream.getNext(mid);
+            if(map != null) {
+                numOfMatches++;
+                System.out.println(map.getColumnLabel());
+            }
+        }
+        while(map != null);
+        */
     }
 
 
-    // Setting the map header constants.
+    // Setting the map header constants. Copied from Minitable.java.
 
     private static short[] setBigTConstants(String dataFileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(dataFileName))) {
@@ -273,21 +274,15 @@ public class IndexStratTest {
 
 
     public static void main(String [] args) throws Exception {
-        init();
         test1();
-        System.out.print("test 1 time: " + test1Time);
-/*
         test2();
-        System.out.print("test 2 time: " + test2Time);
-
         test3();
-        System.out.print("test 3 time: " + test3Time);
-
         test4();
-        System.out.print("test 4 time: " + test4Time);
-
         test5();
-        System.out.print("test 5 time: " + test5Time);
-*/
+        System.out.println("Total time in milliseconds for database creation and batch insertion using strategy 1: " + test1Time/1_000_000);
+        System.out.println("Total time in milliseconds for database creation and batch insertion using strategy 2: " + test2Time/1_000_000);
+        System.out.println("Total time in milliseconds for database creation and batch insertion using strategy 3: " + test3Time/1_000_000);
+        System.out.println("Total time in milliseconds for database creation and batch insertion using strategy 4: " + test4Time/1_000_000);
+        System.out.println("Total time in milliseconds for database creation and batch insertion using strategy 5: " + test5Time/1_000_000);
     }
 }
