b399eae68a9867e5dbed86ee2c92f9fb31ad8770
Madhav Rajesh <32904338+maddyrajesh@users.noreply.github.com>
2023-04-23 21:53:45 -0700

    Implemented GetCounts CLI

---
+++ minjava/javaminibase/src/BigT/GetCounts.java
@@ -0,0 +1,50 @@
+package BigT;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class GetCounts {
+
+    HashSet<String> allBigt;
+    int numBuf;
+
+    public GetCounts(HashSet<String> allBigt, int numBuf) {
+        this.allBigt = allBigt;
+        this.numBuf = numBuf;
+    }
+
+    public void run() throws Exception {
+        int rowCount;
+        int colCount;
+        bigt tempBigt;
+        int mapCount;
+
+        for (String bigtName : allBigt) {
+            System.out.println("Bigtable: " + bigtName);
+            System.out.println("---------------------------------------");
+            tempBigt = new bigt(bigtName, 1);
+            mapCount = tempBigt.getMapCnt();
+            rowCount = tempBigt.getRowCnt();
+            colCount = tempBigt.getColumnCnt();
+            System.out.println("TOTAL NUMBER OF MAPS: " + mapCount);
+            System.out.println("NUMBER OF DISTINCT ROW LABELS: " + rowCount);
+            System.out.println("NUMBER OF DISTINCT COLUMN LABELS: " + colCount);
+            System.out.println("---------------------------------------");
+            tempBigt.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: java GetCounts <NUMBUF> <bigtable1,bigtable2,...>");
+            System.exit(1);
+        }
+
+        int numBuf = Integer.parseInt(args[0]);
+        String[] bigtableNames = args[1].split(",");
+        HashSet<String> allBigt = new HashSet<>(Arrays.asList(bigtableNames));
+
+        GetCounts getAllCount = new GetCounts(allBigt, numBuf);
+        getAllCount.run();
+    }
+}
